e2798b29886f61b513090c6b6598babd878fdc4e
Oleksandr Rabovskyi <orabovskyi@qvantel.com>
2019-11-12 22:32:04 +0200

    3 commit

--- test/strTest.js
+++ test/strTest.js
@@ -9,7 +9,7 @@ const expect = chai.expect;
 
 describe('Several Strings Test', () => {
 	// const testStep = allure.createStep("initial", () => {
-	it('Checks if the strings is reversed 1', () => {
+	it('Checks if the strings is reversed 1 test', () => {
 		let str = strReverse('Mocha is cool!!');
 		expect(str).to.equal('!!looc si ahcoM');
 	  });
@@ -18,13 +18,13 @@ describe('Several Strings Test', () => {
 	// 	testStep();
 	// });
 
-	it('Checks if the strings is reversed 2', () => {
+	it('Checks if the strings is reversed 2 test', () => {
 		let str = strReverse('Chai is super supportive.');
 		expect(str).to.equal('.evitroppus repus si iahC');
 
 	});
 
-	it('Checks if the string return type String', () => {
+	it('Checks if the string return type String 3 test', () => {
 		let str = strCheckType('Chai is super supportive.');
 		// expect(str).to.equal('string'); // можно использовать вариант ниже
 		expect(str).to.be.a('string');
